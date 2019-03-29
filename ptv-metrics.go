e2f518a9f86ea45d99508c8ea4054a1ef4c0bce2
Ian Dykes <ian.dykes@microlise.com>
2019-03-29 17:51:57 +0000

    Added instance metrics

--- ptv-metrics.go
+++ ptv-metrics.go
@@ -42,6 +42,19 @@ type PTVMetrics struct {
 	TimeQuantile95OuterSeconds *PTVMetric
 	TimeQuantile98InnerSeconds *PTVMetric
 	TimeQuantile98OuterSeconds *PTVMetric
+
+	InstanceRestartCounter     *PTVMetric
+	InstanceUserRestartCounter *PTVMetric
+	InstanceUseCounter         *PTVMetric
+
+	InstanceUptimeSeconds         *PTVMetric
+	InstanceProcessCPUTimeSeconds *PTVMetric
+
+	InstanceCommittedVirtualMemoryBytes *PTVMetric
+	InstanceHeapCommittedMemoryBytes    *PTVMetric
+	InstanceHeapUsedMemoryBytes         *PTVMetric
+	InstanceNonHeapCommittedMemoryBytes *PTVMetric
+	InstanceNonHeapUsedMemoryBytes      *PTVMetric
 }
 
 // NewPtvMetrics creates the container of metrics to export
@@ -84,6 +97,19 @@ func NewPtvMetrics() *PTVMetrics {
 		TimeQuantile95OuterSeconds: NewPtvMetric("time_quantile_95_outer_seconds", "Value of outerTime in timeQuantiles for q=0.95 in seconds"),
 		TimeQuantile98InnerSeconds: NewPtvMetric("time_quantile_98_inner_seconds", "Value of innerTime in timeQuantiles for q=0.98 in seconds"),
 		TimeQuantile98OuterSeconds: NewPtvMetric("time_quantile_98_outer_seconds", "Value of outerTime in timeQuantiles for q=0.98 in seconds"),
+
+		InstanceRestartCounter:     NewPtvMetric("instance_restart_counter", "Value of restartCounter for an instance", "instance_name"),
+		InstanceUserRestartCounter: NewPtvMetric("instance_user_restart_counter", "Value of userRestartCounter for an instance", "instance_name"),
+		InstanceUseCounter:         NewPtvMetric("instance_use_counter", "Value of useCounter for an instance", "instance_name"),
+
+		InstanceUptimeSeconds:         NewPtvMetric("instance_uptime_seconds", "Value of uptime in seconds for an instance", "instance_name"),
+		InstanceProcessCPUTimeSeconds: NewPtvMetric("instance_process_cpu_time_seconds", "Value of processCpuTime in seconds for an instance", "instance_name"),
+
+		InstanceCommittedVirtualMemoryBytes: NewPtvMetric("instance_committed_virtual_memory_bytes", "Value of committedVirtualMemorySize for an instance", "instance_name"),
+		InstanceHeapCommittedMemoryBytes:    NewPtvMetric("instance_heap_committed_memory_bytes", "Value of heapCommittedMemorySize for an instance", "instance_name"),
+		InstanceHeapUsedMemoryBytes:         NewPtvMetric("instance_heap_used_memory_bytes", "Value of heapUsedMemorySize for an instance", "instance_name"),
+		InstanceNonHeapCommittedMemoryBytes: NewPtvMetric("instance_non_heap_committed_memory_bytes", "Value of nonHeapCommittedMemorySize for an instance", "instance_name"),
+		InstanceNonHeapUsedMemoryBytes:      NewPtvMetric("instance_non_heap_used_memory_bytes", "Value of nonHeapUsedMemorySize for an instance", "instance_name"),
 	}
 }
 
@@ -122,6 +148,19 @@ func (m *PTVMetrics) Describe(ch chan<- *prometheus.Desc) {
 	ch <- m.TimeQuantile95OuterSeconds.Desc
 	ch <- m.TimeQuantile98InnerSeconds.Desc
 	ch <- m.TimeQuantile98OuterSeconds.Desc
+
+	ch <- m.InstanceRestartCounter.Desc
+	ch <- m.InstanceUserRestartCounter.Desc
+	ch <- m.InstanceUseCounter.Desc
+
+	ch <- m.InstanceUptimeSeconds.Desc
+	ch <- m.InstanceProcessCPUTimeSeconds.Desc
+
+	ch <- m.InstanceCommittedVirtualMemoryBytes.Desc
+	ch <- m.InstanceHeapCommittedMemoryBytes.Desc
+	ch <- m.InstanceHeapUsedMemoryBytes.Desc
+	ch <- m.InstanceNonHeapCommittedMemoryBytes.Desc
+	ch <- m.InstanceNonHeapUsedMemoryBytes.Desc
 }
 
 // SetPtvDown exports metrics indicating that the PTV instance is down
@@ -165,6 +204,22 @@ func (m *PTVMetrics) SetPtvUp(ch chan<- prometheus.Metric, rawMetrics *RawMetric
 	ch <- m.TimeQuantile95OuterSeconds.CreateMetric(prometheus.GaugeValue, millisToSeconds(rawMetrics.GetQuantile(0.95).OuterTime))
 	ch <- m.TimeQuantile98InnerSeconds.CreateMetric(prometheus.GaugeValue, millisToSeconds(rawMetrics.GetQuantile(0.98).InnerTime))
 	ch <- m.TimeQuantile98OuterSeconds.CreateMetric(prometheus.GaugeValue, millisToSeconds(rawMetrics.GetQuantile(0.98).OuterTime))
+
+	for _, rawInstance := range rawMetrics.Instances {
+		ch <- m.InstanceRestartCounter.CreateMetric(prometheus.CounterValue, rawInstance.RestartCounter, rawInstance.InstanceSuffix)
+		ch <- m.InstanceUserRestartCounter.CreateMetric(prometheus.CounterValue, rawInstance.UserRestartCounter, rawInstance.InstanceSuffix)
+		ch <- m.InstanceUseCounter.CreateMetric(prometheus.CounterValue, rawInstance.UseCounter, rawInstance.InstanceSuffix)
+
+		// "upTime" looks like it's in milliseconds, while cpuTime is in nanoseconds?
+		ch <- m.InstanceUptimeSeconds.CreateMetric(prometheus.CounterValue, millisToSeconds(rawInstance.Uptime), rawInstance.InstanceSuffix)
+		ch <- m.InstanceProcessCPUTimeSeconds.CreateMetric(prometheus.CounterValue, cpuTimeToSeconds(rawInstance.ProcessCPUTime), rawInstance.InstanceSuffix)
+
+		ch <- m.InstanceCommittedVirtualMemoryBytes.CreateMetric(prometheus.GaugeValue, rawInstance.CommittedVirtualMemorySize, rawInstance.InstanceSuffix)
+		ch <- m.InstanceHeapCommittedMemoryBytes.CreateMetric(prometheus.GaugeValue, rawInstance.HeapCommittedMemorySize, rawInstance.InstanceSuffix)
+		ch <- m.InstanceHeapUsedMemoryBytes.CreateMetric(prometheus.GaugeValue, rawInstance.HeapUsedMemorySize, rawInstance.InstanceSuffix)
+		ch <- m.InstanceNonHeapCommittedMemoryBytes.CreateMetric(prometheus.GaugeValue, rawInstance.NonHeapCommittedMemorySize, rawInstance.InstanceSuffix)
+		ch <- m.InstanceNonHeapUsedMemoryBytes.CreateMetric(prometheus.GaugeValue, rawInstance.NonHeapUsedMemorySize, rawInstance.InstanceSuffix)
+	}
 }
 
 // cpuTimeToSeconds converts PTV processor time (appears to be nanoseconds) to seconds
